fb6b1a698f35f4314e8cc2243e4bba459e86ea39
Mattias Joëlson <mattias@joelson.org>
2026-04-02 16:54:12 +0200

    Initial setup from spring initializr.

---
+++ src/test/java/org/joelson/linv/LegoInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joelson.linv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
